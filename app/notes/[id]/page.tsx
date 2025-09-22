730fe467a2f31096252c45386f2a80356a384ecd
Vladimir <kremench@gmail.com>
2025-09-22 20:09:03 +0300

    add errors and loading for Details

--- app/notes/[id]/page.tsx
+++ app/notes/[id]/page.tsx
@@ -1,6 +1,8 @@
 "use client";
 import NoteDetails from "@/app/components/NoteDetails/NoteDetails";
+import Error from "@/app/error";
 import { getNotesById } from "@/app/lib/api";
+import Loading from "@/app/loading";
 import { Note } from "@/app/types/notes";
 import { useQuery } from "@tanstack/react-query";
 import { useParams } from "next/navigation";
@@ -10,13 +12,13 @@ const Details = () => {
   const params = useParams();
   const noteId = params.id as string;
 
-  const { data, isLoading, isError } = useQuery<Note>({
+  const { data, isLoading, isError, error } = useQuery<Note>({
     queryKey: ["note", noteId],
     queryFn: () => getNotesById({ noteId }),
     enabled: !!noteId,
   });
-  if (isLoading) return <p>Loading notes</p>;
-  if (isError) return <p>Error loading nite</p>;
+  if (isLoading) return <Loading />;
+  if (isError) return <Error error={error} value="note details" />;
   if (!data) return <p>No note found</p>;
   return (
     <>
